a83e4c34fbdc5a47756280d4f414481969ccee88
Rodwyn <rodwynmoreno@gmail.com>
2020-07-31 11:04:57 -0500

    Added app structure.

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App name="cleaResults" />,
+  document.querySelector('#container')
+);
+
+if (module && module.hot) {
+  module.hot.accept();
+
+  module.hot.addStatusHandler(status => {
+    if (status === 'prepare') console.clear();
+  });
+}

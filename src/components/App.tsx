9dc3762750d00a86611e9d9e2416fd84ee1f69c2
Rodwyn <rodwynmoreno@gmail.com>
2020-07-31 19:22:49 -0500

    Added DataTable component.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Button from '@material-ui/core/Button';
+import DataTable from './DataTable';
 
 interface AppProps {
   name: string;
@@ -20,9 +20,7 @@ export default function App({ name }: AppProps) {
   return (
     <Container>
       <Container padding="1em">Hello {name}!</Container>
-      <Button variant="contained" color="primary">
-        Hola Mundo!
-      </Button>
+      <DataTable></DataTable>
     </Container>
   );
 }
